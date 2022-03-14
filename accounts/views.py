1903dc04549b8beca94708f3e16c2fe0801f5ae8
Anthony <dahekars@gmail.com>
2022-03-14 10:48:38 +0530

    new look

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.contrib.auth import login, authenticate
+from django.http import HttpResponseRedirect
 from .forms import SignUpForm
+from mainserver.models import dispatch_name_1
 
 # Create your views here.
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
+            username = form.cleaned_data.get('username')
+            add_user = dispatch_name_1(name=username, employee_number=form.cleaned_data['employee_number'], email_address=form.cleaned_data['email'])
+            add_user.save()
             form.save()
-
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return HttpResponseRedirect('/srform/')
     else:
         form = SignUpForm()
     return render(request, 'accounts/signup.html', {'form': form})
--- mainserver/views.py
+++ mainserver/views.py
@@ -4,14 +4,32 @@ from .forms import SrForm
 from django.contrib.auth import login
 from django.contrib import messages
 from .models import Srlistofdispatcher, dispatch_name_1
+from django.contrib.auth.models import User
+from datetime import datetime, timedelta
 
 # Create your views here.
 def index(request):
     return render(request, 'mainserver/base.html', {})
 
 def home(request):
-    all_sr = Srlistofdispatcher.objects.all()
-    return render(request, 'mainserver/home.html', {'ls': all_sr})
+    all_data = Srlistofdispatcher.objects.all()
+    return render(request, 'mainserver/home.html', {'ls': all_data})
+
+    #testing
+#    if User.is_authenticated:
+#        if dispatch_name_1.objects.get(name=request.user):
+#            user = request.user
+#            user_name = dispatch_name_1.objects.get(name=user)
+#            today = datetime.today() - timedelta(days=1)
+#            all_sr_for_today = Srlistofdispatcher.objects.filter(dispatch_name_of_sr= user_name, date_created__gte=today) 
+#            return render(request, 'mainserver/home.html', {'ls': all_sr_for_today})
+#        if not dispatch_name_1.objects.get(name=request.user):
+#            all_data = Srlistofdispatcher.objects.all()
+#            return render(request, 'mainserver/home.html', {'ls': all_data})
+#    
+#    elif User.is_anonymous:
+#        all_data = Srlistofdispatcher.objects.all()
+#        return render(request, 'mainserver/home.html', {'ls': all_data})
 
 
 def srform_page(request):
@@ -23,9 +41,15 @@ def srform_page(request):
             #dispatcher = form.cleaned_data['dispatch_name_of_sr']
             action_taken = form.cleaned_data['action_taken']
 
-            add_process = Srlistofdispatcher(sr_number=sr, serivce_tag=service_tag, action_taken=action_taken, dispatch_name_of_sr= dispatch_name_1.objects.get(id = 1))
+            add_process = Srlistofdispatcher(sr_number=sr, serivce_tag=service_tag, action_taken=action_taken, dispatch_name_of_sr= dispatch_name_1.objects.get(name=request.user))
             add_process.save()
+
         return HttpResponseRedirect('/srform/')
     else:
         form = SrForm()
-    return render(request, 'mainserver/srform.html', {'form': form})
+    
+    user = request.user
+    user_name = dispatch_name_1.objects.get(name=user)
+    today = datetime.today() - timedelta(days=1)
+    latest_5 =Srlistofdispatcher.objects.filter(dispatch_name_of_sr= user_name, date_created__gte=today) [:5]
+    return render(request, 'mainserver/srform.html', {'form': form, 'latest_5': latest_5})
